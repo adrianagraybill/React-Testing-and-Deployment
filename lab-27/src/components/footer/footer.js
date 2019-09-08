c740838783e07d47315ce3971988db23e6d16e02
adrianagraybill <adriana.graybill@gmail.com>
2019-09-07 18:51:07 -0700

    done

---
+++ lab-27/src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.scss";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <h3>&copy; 2018 Code Fellows</h3>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
